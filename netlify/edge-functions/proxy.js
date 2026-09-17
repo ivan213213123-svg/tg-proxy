026509167a13e741bd30d565cde4757368bb8753
ivan213213123-svg <ivan213213123@gmail.com>
2026-09-17 14:43:36 +0300

    Create proxy.js

---
+++ netlify/edge-functions/proxy.js
@@ -0,0 +1,12 @@
+export default async (request) => {
+  const url = new URL(request.url);
+  const target = "https://api.telegram.org" + url.pathname + url.search;
+  const init = {
+    method: request.method,
+    headers: request.headers,
+    body: ["GET", "HEAD"].includes(request.method) ? undefined : request.body,
+  };
+  return fetch(target, init);
+};
+
+export const config = { path: "/*" };
